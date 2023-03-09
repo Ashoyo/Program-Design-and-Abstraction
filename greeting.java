a2dc759eba03979a6f3c6fe5642066e770fe82ec
Shoyo Jiang <81840394+Ashoyo@users.noreply.github.com>
2023-03-09 17:49:51 -0500

    Create greeting.java

---
+++ greeting.java
@@ -0,0 +1,11 @@
+class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!"); 
+        System.out.println("Good morning, Professor Crotty"); 
+        System.out.println("My name is Shoyo Jiang"); 
+        System.out.println("Today's date is 1/18/2022"); 
+        System.out.println("This program consist of varieties of print statement 
+which prints my comments"); 
+        
+    }
+}
